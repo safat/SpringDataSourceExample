9f18dbc4763d92d2166bfa18283e696caaf95313
safat113 <shakhawat.hossain@therapservices.net>
2014-06-11 12:48:16 +0600

    jpa..

--- src/main/java/net/therap/dao/UserDao.java
+++ src/main/java/net/therap/dao/UserDao.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,26 +22,14 @@ import java.util.List;
 public class UserDao {
 
     @PersistenceContext
-    private EntityManager em;
-
-    @Autowired
-    private SessionFactory sessionFactory;
-
-    public void setSessionFactory(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
-
-    public void insertUser(User user) {
-        Session session = sessionFactory.getCurrentSession();
-        session.save(user);
-    }
+    private EntityManager entityManager;
 
     public List<User> getUserList() {
 //        Session session = sessionFactory.getCurrentSession();
 //        Criteria criteria = session.createCriteria(User.class);
 //        List<User> userList = criteria.list();
 //        return userList;
-        List<User> userList = em.createQuery("FROM User").getResultList();
+        List<User> userList = entityManager.createQuery("FROM User AS U where U.name = 'user' ").getResultList();
         return userList;
     }
 }
